aec595b1476b79b73eb8e86e33988deaf3d474f0
samuelhklumpers <34067057+samuelhklumpers@users.noreply.github.com>
2022-08-06 11:36:05 +0200

    Update menuserver.py
    menuserver: take optional port argument

--- server/menuserver.py
+++ server/menuserver.py
@@ -4,6 +4,9 @@ import socketserver
 from functools import partial
 
 PORT = 8080
+if len(sys.argv) > 1:
+    PORT = int(sys.argv[1])
+    
 handler = partial(http.server.SimpleHTTPRequestHandler, directory="./menu")
 
 with socketserver.TCPServer(("", PORT), handler) as http_server:
